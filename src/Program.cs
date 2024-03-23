15bef4c505b87906418bddc8c93c16ad96bed63b
NoufSalehA <noufsalehc23@gmail.com>
2024-03-23 17:04:09 +0300

    Challenge 3 : guessing game

--- src/Program.cs
+++ src/Program.cs
@@ -4,35 +4,79 @@ namespace FunctionChallenges
 {
     class Program
     {
-        public static void StringNumberProcessor(params object[] inputs)
-        {
-            string connactionString = " ";
-            int sum = 0;
-            foreach (var input in inputs)
-            {
-                if (input is string)
-                {
-                    connactionString += input + " ";
 
-                }
-                else
+        public static void GuessingGame()
+        {
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1000);
+            int guesses = 0;
+            bool isItCorecct = false;
+
+            while (!isItCorecct)//true
+            {
+                Console.WriteLine("Enter a number from (1-1000), type quit to exit");
+                string input = Console.ReadLine() ?? "";
+
+                if (input != null)
                 {
-                    sum += (int)input;
+
+                    if (input.ToLower() == "quit")
+                    {
+                        Console.WriteLine("===============Quit The Game============");
+                        break;
+
+                    }
+                }
+                try
+                {
+
+                    if (int.TryParse(input, out int guess))
+                    {
+                        guesses++;
+
+
+                        if (guess == randomNumber)
+                        {
+
+                            Console.WriteLine("****You guessed the right number !****");
+                            isItCorecct = true;
+
+
+                        }
+                        else if (guess < randomNumber)
+                        {
+
+                            Console.WriteLine("===Too small!===");
+
+                        }
+                        else if (guess > randomNumber)
+                        {
+                            Console.WriteLine("===Too Big!===");
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Invalid Value :{e.Message}");
 
                 }
 
             }
-Console.WriteLine($"{connactionString} ; {sum}");
 
 
         }
         static void Main(string[] args)
         {
-            // Challenge 1: String and Number Processor
-            Console.WriteLine("Challenge 1: String and Number Processor");
-            StringNumberProcessor("Hello", 100, 200, "World"); // Expected outcome: "Hello World; 300"
 
-            
+            //Challenge 3: Guessing Game
+            // Uncomment to test the GuessingGame method
+            // Expected outcome: User input until the correct number is guessed or user inputs `Quit`
+            Console.WriteLine($"Welcome !\nChallenge 3: Guessing Game");
+            GuessingGame();
+
+
         }
     }
 }
